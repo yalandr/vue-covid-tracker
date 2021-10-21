4514250d6c2658059fb94a9d998c8356eba127f1
yalandr <52127093+yalandr@users.noreply.github.com>
2021-10-21 20:59:15 +0300

    Add files via upload

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="country-select-container">
+        <select v-model="selected" @change="countrySelectMethod()" class="country-select">
+            <option value="0">Select Country</option>
+            <option v-for="country in countries" :value="country.ID">
+                {{ country.Country }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        countrySelectMethod() {
+            const country = this.countries.find((item) => item.ID === this.selected);
+
+            this.$emit('getCountry', country);
+        }
+    }
+}
+</script>
+
+<style>
+.country-select-container {
+}
+.country-select {
+    width: 100%;
+    max-width: 300px;
+    height: 40px;
+    padding: 8px;
+    font-size: 1rem;
+    color: #555;
+    outline: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+.country-select:focus {
+    border: 2px solid #145DBF;
+}
+.country-select option {
+    background-color: rgba(0,0,0,.7);
+    color: #eee;
+}
+</style>
---
+++ src/components/DataTitle.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="data-title-container">
+        <h2 class="data-title">{{text}}</h2>
+        <div class="data-date">
+            {{timestamp}}
+        </div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+    name: 'DataTitle',
+    props: ['text', 'dataDate'],
+    computed: {
+        timestamp: function() {
+            return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+        }
+    }
+}
+</script>
+
+<style>
+.data-title-container, .data-title {
+    text-align: center;
+}
+.data-title {
+    color: #145DBF;
+    margin-bottom: 0.5rem;
+    font-weight: 300;
+    font-size: 2.2rem;
+}
+.data-date {
+    font-size: 0.9rem;
+    opacity: .7;
+}
+</style>
